4f9223b5032cdb434e713b76f5255c64c75c91d0
Jonathan Lee <jcl4.jonathan@gmail.com>
2023-06-18 01:08:08 -0700

    fixed tailwind and added example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Welcome to My Website!
-    </h1>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
   );
 }
 
