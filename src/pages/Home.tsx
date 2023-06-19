0fff8ae302e667c805dec71bff42a73016083e7d
Jonathan Lee <jcl4.jonathan@gmail.com>
2023-06-19 01:09:18 -0700

    added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home(): JSX.Element {
+  return (
+    <section className="h-screen bg-transparent flex flex-col content-center justify-center text-center p-0">
+      <p className="text-6xl before:horizontal-line after:horizontal-line">Hey, I'm</p>
+      <p className="text-9xl">Jonathan Lee</p>
+      <p className="text-5xl">Software Engineer &#183; Roboticist &#183; Mechanical Engineer</p>
+    </section>
+  );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'home-background': "url('./assets/home-background.JPG')",
+      }
+    },
   },
   plugins: [],
 }
